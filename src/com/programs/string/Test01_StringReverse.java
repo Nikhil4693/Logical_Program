9ccb6206ae3f4038fbb9d4601d4c078948f2b0b9
Nikhil Wadatkar <nikhil.wadatkar4693@gmail.com>
2022-03-29 03:57:08 +0530

    Folder added

---
+++ src/com/programs/string/Test01_StringReverse.java
@@ -0,0 +1,19 @@
+package com.programs.string;
+
+public class Test01_StringReverse {
+
+	public static void main(String a[]) {
+		String str = "Java 2 career";
+		String arr[] = str.split("");
+		System.out.print("Reversed String of " + str + ":");
+		for (int i = arr.length - 1; i >= 0; i--) {
+			char temp[] = arr[i].toCharArray();
+			for (int j = temp.length - 1; j >= 0; j--) {
+				System.out.print(temp[j]);
+
+			}
+			System.out.print("");
+		}
+	}
+
+}
